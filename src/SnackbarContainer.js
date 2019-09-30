532453e295ce33622fd2a77db76b123148474fa0
Germán Martínez <germaga92@gmail.com>
2019-09-30 06:27:04 +0200

    Update SnackbarContainer top style

--- src/SnackbarContainer.js
+++ src/SnackbarContainer.js
@@ -25,7 +25,7 @@ const styles = theme => ({
     },
     reverseColumns: { flexDirection: 'column-reverse' },
 
-    top: { top: 45 },
+    top: { top: 58 },
     topDense: { top: SNACKBAR_INDENTS.view.dense - SNACKBAR_INDENTS.snackbar.dense },
 
     bottom: { bottom: SNACKBAR_INDENTS.view.default - SNACKBAR_INDENTS.snackbar.default },
